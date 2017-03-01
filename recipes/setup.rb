47fd106456541abbe80d7c803945829bc343522b
vagrant <vagrant@localhost.localdomain>
2017-03-01 03:56:33 +0000

    demoing hard coding motd with sysparams

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -9,8 +9,11 @@ package	'tree' do
 end
 
 file '/etc/motd' do
- content 'This setup file belongs to Nick R.' 
- owner 'root'
+ content 'This setup file belongs to Nick R.
+ HOSTNAME: localhost.localdomain
+ IPADDRESS: 10.0.2.15
+ CPU: ~2800 MHz'
+owner 'root'
  group 'root'
 end
 
