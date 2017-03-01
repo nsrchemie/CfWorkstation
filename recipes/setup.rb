0a985bfd60941a81f3414fd79bf67ccbc29ce2bd
vagrant <vagrant@localhost.localdomain>
2017-03-01 04:14:36 +0000

    added dnyamic node parameter detection and output in motd by interpolating with ohai

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -8,12 +8,15 @@ package	'tree' do
  action	:install
 end
 
+
 file '/etc/motd' do
- content 'This setup file belongs to Nick R.
- HOSTNAME: localhost.localdomain
- IPADDRESS: 10.0.2.15
- CPU: ~2800 MHz'
-owner 'root'
+ content "This server file belongs to nsrchemie.
+ HOSTNAME: #{node['hostname']}
+ IPADDRESS: #{node['ipaddress']}
+ CPU: #{node['cpu']['0']['mhz']}
+ MEMORY: #{node['memory']['total']}
+"
+ owner 'root'
  group 'root'
 end
 
